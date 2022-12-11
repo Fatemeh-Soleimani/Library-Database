ad2fb48eb6aac654d68e25d4a931d6b99981584c
Fatemeh-Soleimani <fsoleimani79@yahoo.com>
2022-12-11 17:17:51 +0330

    edit tables key

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,27 +1,6 @@
-create table Book(
-	bookID varchar(5),
-	title varchar(30) not null,
-	publisherName varchar(20) not null,
-	branchName varchar(5),
-	gradeID varchar(10),
-	categoryID varchar(20),
-	penalty float,
-	primary key (bookID),
-	foreign key (publisherName) references publisher,
-	foreign key (bookID) references loans,
-	foreign key (bookID) references Authors,
-	foreign key (bookID) references copies,
-	foreign key (categoryID) references ctegory,
-
-);
-
-create table Authors(
-	bookID varchar(5) not null,
-	Name varchar(40) not null,
-	nationality varchar(20),
 
 
-);
+
 
 create table publisher(
 	publisherName varchar(20) not null,
@@ -32,11 +11,6 @@ create table publisher(
 
 );
 
-create table copies(
-	bookID varchar(5) not null,
-	numOfCopies int,
-);
-
  create table category(
 	categoryID varchar(20) primary key,
 	name varchar(50),
@@ -44,14 +18,7 @@ create table copies(
 
  );
 
- create table loans(
-	bookID varchar(5) not null,
-	userID varchar(10) not null,
-	returnDate char(10),
-	dateOut char(10),
-	isReturned int check (isReturned in(0,1)),
-	numDays int,
- );
+ 
 
  create table Member(
 	userID varchar(10) primary key,
@@ -61,6 +28,43 @@ create table copies(
 	registrationDate varchar(10),
 	gender varchar(8) check (gender in ('female','male')),
 	dateOfBirth varchar(10),
-	foreign key (categoryID), references category,
-	foreign key (userID) references loans,
- )
+	foreign key (categoryID) references category,
+	
+ );
+ create table Book(
+	bookID varchar(5),
+	title varchar(30) not null,
+	publisherName varchar(20) not null,
+	branchName varchar(5),
+	gradeID varchar(10),
+	categoryID varchar(20),
+	penalty float,
+	primary key (bookID),
+	foreign key (publisherName) references publisher,
+	foreign key (categoryID) references ctegory,
+
+);
+
+create table copies(
+	bookID varchar(5) not null,
+	numOfCopies int,
+	foreign key (bookID) references Book,
+);
+
+create table Authors(
+	bookID varchar(5) not null,
+	Name varchar(40) not null,
+	nationality varchar(20),
+	foreign key (bookID) references Book,
+
+);
+create table loans(
+	bookID varchar(5) not null,
+	userID varchar(10) not null,
+	returnDate char(10),
+	dateOut char(10),
+	isReturned int check (isReturned in(0,1)),
+	numDays int,
+	foreign key (bookID) references Book,
+	foreign key (userID) references Member,
+ );
